3cedaba279708158266124ba22e81e6a19de16cf
williamSabino <96441509+williamSabino@users.noreply.github.com>
2024-03-13 14:30:26 -0300

    init

---
+++ src/main/java/br/com/will/macdonalts/MacdonaltsApplication.java
@@ -0,0 +1,13 @@
+package br.com.will.macdonalts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacdonaltsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacdonaltsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/will/macdonalts/MacdonaltsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.will.macdonalts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacdonaltsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
